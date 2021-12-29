0d8f9a6bf2784c2e2792ff4d4f32a44232c5dc6f
I502147 <arpith.t@sap.com>
2021-12-29 12:15:40 +0530

    modularized code

--- aws_iat.py
+++ aws_iat.py
@@ -2,7 +2,7 @@ import requests
 import json
 from influxdb import InfluxDBClient
 from datetime import datetime
-
+#
 
 def aws_cf_oauth_token():
     url = "https://uaa.cf.sap.hana.ondemand.com/oauth/token"
@@ -14,19 +14,11 @@ def aws_cf_oauth_token():
     }
 
     response = requests.request("POST", url, headers=headers, data=payload)
+    response.raise_for_status()
 
     aws_token = response.json()
 
     return aws_token["access_token"]
-
-
-dev_client = InfluxDBClient('hci-rit-prism-sel.cpis.c.eu-de-2.cloud.sap', 8086, 'arpdb')
-dev_client.switch_database('arpdb')
-
-app_list = ["it-co", "it-trm", "it-gb", "it-app", "it-rootwebapp", "it-design-service", "it-app-prov", "it-km-rest",
-            "it-op-rest", "it-op-odata", "it-op-consumer", "it-is-app-prov-route", "it-runtime-api", "it-tpm",
-            "it-tlm-service", "it-op-consumer-b2b", "it-op-jobs", "it-op-jobs-archiving", "it-op-odata-batch-router",
-            "it-op-static", "it-op-static-b2b"]
 
 
 def cpi_prod_version():
@@ -54,51 +46,69 @@ def cpi_prod_version():
     return cpi_product_version
 
 
-for app in app_list:
-    url = f"https://api.cf.sap.hana.ondemand.com/v3/apps?page=1&per_page=1000&space_guids=2c92d3e7-a833-4fbf-89e2" \
-          f"-917c07cea220&names={app}"
+def push_app_info():
+    dev_client = InfluxDBClient('hci-rit-prism-sel.cpis.c.eu-de-2.cloud.sap', 8086, 'arpdb')
+    dev_client.switch_database('arpdb')
 
-    payload = {}
-    headers = {
-        'Authorization': f'Bearer {aws_cf_oauth_token()}'
-    }
+    app_list = ["it-co", "it-trm", "it-gb", "it-app", "it-rootwebapp", "it-design-service", "it-app-prov", "it-km-rest",
+                "it-op-rest", "it-op-odata", "it-op-consumer", "it-is-app-prov-route", "it-runtime-api", "it-tpm",
+                "it-tlm-service", "it-op-consumer-b2b", "it-op-jobs", "it-op-jobs-archiving",
+                "it-op-odata-batch-router",
+                "it-op-static", "it-op-static-b2b"]
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+    for app in app_list:
+        url = f"https://api.cf.sap.hana.ondemand.com/v3/apps?page=1&per_page=1000&space_guids=2c92d3e7-a833-4fbf-89e2" \
+              f"-917c07cea220&names={app}"
 
-    app_info = response.json()
+        payload = {}
+        headers = {
+            'Authorization': f'Bearer {aws_cf_oauth_token()}'
+        }
 
-    # print(app_info)
+        response = requests.request("GET", url, headers=headers, data=payload)
 
-    for application in app_info["resources"]:
-        # print(application)
-        app_name = app_info["resources"][0]["name"]
-        app_state = app_info["resources"][0]["state"]
-        app_version = app_info["resources"][0]["metadata"]["annotations"]["mta_version"]
-        app_updated_at = app_info["resources"][0]["updated_at"]
-        app_guid = app_info["resources"][0]["guid"]
+        app_info = response.json()
 
-        # print(f"App->{app_name}\nStatus-{app_state}\nApp-Version-{app_version}\nUpdatedOn-{app_updated_at}\nGUID-{app_guid}\n\n")
+        # print(app_info)
 
-        all_app_info = [
-            {
-                "measurement": "MTMS_INFO",
-                "tags": {
-                    "IAAS": "AWS",
-                    "Product_Version": f"{cpi_prod_version()} - {datetime.now().date()}"
-                },
-                "fields": {
-                    "Microservice": app_name,
-                    "Status": app_state,
-                    "Version": app_version,
-                    "UpdatedOn": app_updated_at,
-                    "GUID": app_guid
+        for application in app_info["resources"]:
+            # print(application)
+            app_name = app_info["resources"][0]["name"]
+            app_state = app_info["resources"][0]["state"]
+            app_version = app_info["resources"][0]["metadata"]["annotations"]["mta_version"]
+            app_updated_at = app_info["resources"][0]["updated_at"]
+            app_guid = app_info["resources"][0]["guid"]
+
+            # print(f"App->{app_name}\nStatus-{app_state}\nApp-Version-{app_version}\nUpdatedOn-{app_updated_at}\nGUID-{app_guid}\n\n")
+
+            all_app_info = [
+                {
+                    "measurement": "MTMS_INFO",
+                    "tags": {
+                        "IAAS": "AWS",
+                        "Product_Version": f"{cpi_prod_version()} - {datetime.now().date()}"
+                    },
+                    "fields": {
+                        "Microservice": app_name,
+                        "Status": app_state,
+                        "Version": app_version,
+                        "UpdatedOn": app_updated_at,
+                        "GUID": app_guid
+                    }
                 }
-            }
-        ]
-        # print(all_app_info)
-        if dev_client.write_points(all_app_info, protocol='json'):
-            print("Data Insertion success")
-            pass
-        else:
-            print("Dev-Data Insertion Failed")
-            print(all_app_info)
+            ]
+            # print(all_app_info)
+            if dev_client.write_points(all_app_info, protocol='json'):
+                print(f"{app_name} Data Insertion success")
+                pass
+            else:
+                print(f"{app_name} Dev-Data Insertion Failed")
+                print(all_app_info)
+
+def main():
+    aws_cf_oauth_token()
+    cpi_prod_version()
+    push_app_info()
+
+if __name__ == "__main__":
+    main()
