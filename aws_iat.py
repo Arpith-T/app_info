499a90e84d0307e574295b41afa48a176e12e754
I502147 <arpith.t@sap.com>
2022-01-03 20:43:53 +0530

    aws name changed

--- aws_iat.py
+++ aws_iat.py
@@ -91,7 +91,7 @@ def push_app_info():
                 {
                     "measurement": "MTMS_INFO",
                     "tags": {
-                        "IAAS": "AWS",
+                        "IAAS": "AWS-IAT",
                         "Product_Version": f"{cpi_prod_version()} - {datetime.now().date()}"
                     },
                     "fields": {
